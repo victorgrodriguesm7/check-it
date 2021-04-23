d6653b49986ed0f277e228484d482e288bbfe00e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-22 22:07:40 -0300

    Privated Routed Updated

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 
-export default function PrivateRoute({ component: Component, ...rest}: any) {
-    const { user } = useAuth();
 
+export default function PrivateRoute({ component: Component, ...rest}: any) {
+    const { authenticated } = useAuth();
+
+    if (authenticated)
+        return <Route {...rest} render={props => <Component {...props}/> }/>
+    
     return (
-        user ?  
-            <Route {...rest} render={props => {<Component {...props} />}}/> 
-            : <Redirect to="/login"/>
-    )
+        <Redirect to="/login"/>
+    );
 }
