adadfb1553f275c2827e83a517827ecaf770d813
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-22 22:05:06 -0300

    Created Dashboard css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Topic from '../../components/Topic'
+import TaskProvider from '../../contexts/TaskContext';
+import './index.css';
 
 export default function Dashboard() {
     return (
-        <div>
-            DashBoard
-        </div>
+        <TaskProvider>
+            <Header/>
+            <main className="content">
+                <Topic type="Pendente"/>
+                <Topic type="Em Andamento"/>
+                <Topic type="Finalizado"/>
+                <Topic type="Cancelado"/>
+            </main>
+        </TaskProvider>
     )
 }
