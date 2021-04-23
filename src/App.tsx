d9ecb7eb851bae81cdc87fd2330141bd43bb07ab
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-23 16:03:11 -0300

    Change Provider Position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AuthProvider from './contexts/AuthContext';
+import TaskProvider from './contexts/TaskContext';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -7,12 +8,18 @@ import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
+function DashWithProvider(){
+  return <TaskProvider> 
+    <Dashboard/> 
+  </TaskProvider>
+}
+
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <PrivateRoute exact path="/" component={Dashboard}/>
+            <PrivateRoute exact path="/" component={DashWithProvider}/>
           <Route path="/login" component={LoginPage}/>
           <Route path="/signup" component={SignupPage}/>
         </Switch>
