7c32cf577dcbbd3b7e049854b833d7fba3f0066e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-26 11:40:00 -0300

    Added FilterProvider to FilterPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AuthProvider from './contexts/AuthContext';
 import TaskProvider from './contexts/TaskContext';
+import FilterProvider from './contexts/FilterContext';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import FilterPage from './pages/FilterPage';
 
 function DashWithProvider(){
   return <TaskProvider> 
@@ -14,12 +16,22 @@ function DashWithProvider(){
   </TaskProvider>
 }
 
+function FilterWithProvider(){
+  return (
+    <TaskProvider> 
+      <FilterProvider>
+        <FilterPage/>
+      </FilterProvider>
+    </TaskProvider>
+  );
+}
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-            <PrivateRoute exact path="/" component={DashWithProvider}/>
+          <PrivateRoute exact path="/" component={DashWithProvider}/>
+          <PrivateRoute path="/filter" component={FilterWithProvider}/>
           <Route path="/login" component={LoginPage}/>
           <Route path="/signup" component={SignupPage}/>
         </Switch>
