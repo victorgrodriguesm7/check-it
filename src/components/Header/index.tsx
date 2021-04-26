d225a8214fb80fc0971828df5770fc2e38097b16
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-26 11:58:21 -0300

    Removed Header Filter options

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,49 +1,19 @@
-import React, { useState } from 'react'
+import React  from 'react'
+import { useHistory } from 'react-router';
 import { useAuth } from '../../contexts/AuthContext';
 import { useTasks } from '../../contexts/TaskContext';
-import ComboBox from '../ComboBox';
 import './index.css';
 
+interface HeaderProps {
+    type: 'Dashboard' | 'Filter';
+}
+export default function Header({ type }: HeaderProps) {
+    const { displayName, logout} = useAuth();
+    const { openRegisterModal } = useTasks();
+    const history = useHistory();
 
-type Type =  "Pendente" | "Em Andamento" | "Finalizado" | "Cancelado";
-export default function Header() {
-    const status: Array<Type> = ["Pendente","Em Andamento","Finalizado","Cancelado"];
-    const { displayName, logout} = useAuth()
-    const { filter, changeFilter, clearFilter, openRegisterModal } = useTasks()
-    const [ isFiltrarSelected, setIsFiltrarSelected ] = useState(false);
-    
-    function handleStatusClick(status: Type){
-        function sortByStatusType(status: Array<Type>){
-            let preOrder = ["Pendente" ,"Em Andamento" ,"Finalizado" ,"Cancelado"]
-            let newArray = new Array(preOrder.length);
-            for (var i = 0;i <= status.length;i++){
-                let j = preOrder.indexOf(status[i])
-                newArray[j] = status[i]
-            }
-    
-            return newArray.filter((value) => value != null);
-
-        }
-        if (filter.status?.filter((value) => value === status).length){
-            return ;
-        }
-        
-        let newFilter;
-
-        if (!filter.status){
-            newFilter = [ status ]
-        } else {
-            newFilter = sortByStatusType(filter.status.concat([ status]));
-        }
-
-        changeFilter({
-            onwer: null,
-            status: newFilter
-        });
-    }
-
-    function handleCleanFilter(event: React.MouseEvent<HTMLButtonElement>){
-        clearFilter(true);
+    function handleFilter(){
+        history.push('/filter');
     }
 
     return (
@@ -64,53 +34,31 @@ export default function Header() {
                     <img src="icons/plus.svg" alt="Plus Icon"/>
                     <h2> Adicionar Tarefa </h2>
                 </button>
-                <div className="filter">
-                    <button 
-                        type="button" 
-                        className={"filter-tasks" + (isFiltrarSelected ? " active" : "")} 
-                        onClick={(e) => setIsFiltrarSelected(!isFiltrarSelected)}>
-                        <h2> Filtrar </h2>
-                        <div className="box">
-                            <img src="icons/arrow.svg" alt="Plus Icon"/>
-                        </div>
-                    </button>
-                    <div className="filter-options">
-                        <div className="by-status">
-                            <p className="option">por Status:</p>
-                            <div className="status-container">
-                               {
-                                   status.map(( state: Type ) => {
-                                       let classe = "status"
-                                       if (filter.status?.length ?? 0){
-                                            let value = filter.status!.filter((value) => value === state);
-
-                                            if (value.length){
-                                                classe += ' active'
-                                            }
-                                        } 
-
-                                        return (
-                                            <button 
-                                                type="button" 
-                                                className={classe} 
-                                                key={state}
-                                                onClick={(e) => handleStatusClick(state)}>
-                                                { state }
-                                            </button>
-                                        )
-                                   })
-                               }
+                {
+                type === "Dashboard" ?
+                    <div className="filter">
+                        <button 
+                            type="button" 
+                            className="filter-tasks"
+                            onClick={(e) => handleFilter()}
+                            >
+                            <h2> Filtrar </h2>
+                            <div className="box">
+                                <img src="icons/search.svg" alt="Plus Icon"/>
                             </div>
-                        </div>
-                        <div className="by-member">
-                            <p className="option">por Membro:</p>
-                            <ComboBox/>
-                        </div>
-                        <button type="button" className="clean" onClick={handleCleanFilter}>
-                          Limpar Filtro
                         </button>
                     </div>
-                </div>
+                    :
+                    <div className="return">
+                        <button
+                            type="button"
+                            className="return-button"
+                            onClick={(e) => history.push('/')}
+                            >
+                            <h2>Retornar para Home</h2>
+                        </button>
+                    </div>
+                }
             </div>
         </header>
     )
